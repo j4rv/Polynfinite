c8d7226424506808f9f0ec6151b3fd30ed932a3e
Jarv <kira-8@hotmail.com>
2017-09-07 17:46:28 +0200

    Changing default options

--- Polynfinite/Options.py
+++ Polynfinite/Options.py
@@ -1,4 +1,3 @@
-import sys
 from Polynfinite import Colors
 
 #Image options
@@ -6,12 +5,12 @@ FINAL_SIZE_X = 1920
 FINAL_SIZE_Y = 1080
 OVERSAMPLING = 2                    #For smoother results (without aliasing).
 BACKGROUND_COLOR = Colors.BLACK
-IMAGES_TO_CREATE = 6
+IMAGES_TO_CREATE = 10
 SAVE_FOLDER = "./renders/"
 
 #Polygon options
-LINE_WIDTH = OVERSAMPLING           #Integer numbers only.
+LINE_WIDTH = 2                      #Integer numbers only.
 RATIO = 0.01                        #From zero to one.
-ITERATIONS = 100
-MIN_VERTICES = 3
-MAX_VERTICES = 6
+ITERATIONS = 500
+MIN_VERTICES = 4
+MAX_VERTICES = 10
