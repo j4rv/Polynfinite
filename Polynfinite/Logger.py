b75244c29fa2f145377553dc858d71c5fe7d21a9
Jarv <6jarv91@gmail.com>
2017-09-07 17:13:16 +0200

    Improved logged

--- Polynfinite/Logger.py
+++ Polynfinite/Logger.py
@@ -1,15 +1,15 @@
 import logging
 
+
+logging.basicConfig(filename='log.log', level=logging.INFO, format="[%(levelname)s]: %(message)s")
 def logSavedImage(fileName, sizeX, sizeY, oversampling, backgroundColor, polygon, lineWidth, ratio, iterations):
     print("Saved image: " + fileName)
 
-    logging.basicConfig(filename='log.log', level=logging.INFO)
     logging.info('    ----    {filename}    ----    '.format(filename=fileName))
-    logging.info('Size: {sizex}x{sizey}.'.format(sizex=sizeX, sizey=sizeY))
+    logging.info('Size: {sizex}x{sizey}'.format(sizex=sizeX, sizey=sizeY))
     logging.info('Background color: {bgcolor}'.format(bgcolor=backgroundColor))
     logging.info('Oversampling: {oversampling}'.format(oversampling=oversampling))
     logging.info('Polygon used: {polygon}'.format(polygon=polygon))
-    logging.info('Line width: {lineWidth}.'.format(lineWidth=lineWidth))
-    logging.info('Ratio: {ratio}.'.format(ratio=ratio))
-    logging.info('Iterations: {iterations}.'.format(iterations=iterations))
-    logging.info('\n')
+    logging.info('Line width: {lineWidth}'.format(lineWidth=int(lineWidth)))
+    logging.info('Ratio: {ratio}'.format(ratio=ratio))
+    logging.info('Iterations: {iterations}\n'.format(iterations=iterations))
